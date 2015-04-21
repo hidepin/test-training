b52e286200da811edbd137bba42edddc0885bb19
Hidetoshi IMAI <hidepin@gmail.com>
2015-04-21 23:59:16 +0900

    rails template initial add.

---
+++ ror-training/config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+RorTraining::Application.config.secret_key_base = 'd549b8187c1c8514a080e95dc6d58d4b30b01f21a0f4de6ce0a4efc96aa12bd3418253f1a704db6dbc6e838fc61e0238ae2995217bdaf7494c00dcbd8b55a606'
---
+++ ror-training/config/initializers/session_store.rb
@@ -0,0 +1,3 @@
+# Be sure to restart your server when you modify this file.
+
+RorTraining::Application.config.session_store :cookie_store, key: '_ror-training_session'
